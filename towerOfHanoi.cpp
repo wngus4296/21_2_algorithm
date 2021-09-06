5dac6a1a0bd8d3650c5187e37cfec8fd28f0c6ef
변주현 <wngus4296@swu.ac.kr>
2021-09-06 21:30:20 +0900

    comment: 주석 추가

--- towerOfHanoi.cpp
+++ towerOfHanoi.cpp
@@ -7,15 +7,15 @@ void Hanoi(int n, char from, char temp, char to)
 // n : 원반개수, from : 원래 위치, temp : 임시 장소, to :목적지
 {
 	// cnt = pow(2, n) - 1;
-	// 가장 밑에 있는 원반을 제외한 모든 원반은 from -> temp, temp -> to로 옮겨진다.
+	// 가장 밑에 있는 원반을 제외한 모든 원반은 from -> temp -> to로 옮겨진다.
 
 	if (n == 1) {
 		cout << "#" << ++cnt << ".디스크 1개를 " << from << "지점에서 " << to << "로 옮긴다." << endl;
 	}
 	else {
-		Hanoi(n - 1, from, to, temp);
-		cout << "#" << ++cnt << ".디스크 1개를 " << from << "지점에서 " << to << "로 옮긴다." << endl;
-		Hanoi(n - 1, temp, from, to);
+		Hanoi(n - 1, from, to, temp); // 맨 밑 원반을 제외한 모든 원반을 from에서 temp로 옮긴다.
+		cout << "#" << ++cnt << ".디스크 1개를 " << from << "지점에서 " << to << "로 옮긴다." << endl; // 맨 밑 원반을 from에서 to로 옮긴다.
+		Hanoi(n - 1, temp, from, to); // 맨 밑 원반을 제외한 모든 원반을 temp에서 to로 옮긴다.
 	}
 }
 
