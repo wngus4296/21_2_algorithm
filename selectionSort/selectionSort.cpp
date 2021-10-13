587b8f55892a62d52d208f7ae87c66555ccbd0b8
juhyeon <wngus4296@swu.ac.kr>
2021-10-13 15:36:49 +0900

    feat: selectionSort 생성

--- selectionSort/selectionSort.cpp
+++ selectionSort/selectionSort.cpp
@@ -14,8 +14,17 @@ void print_data()
 
 void sort()
 {
-	/* 이 곳에 알맞은 내용을 추가하시오. */
-	/* 입력 데이타는 L[]에 들어 있으면, 전체 원소 개수는 SIZE이다. */
+	for (int i = SIZE - 1; i > 0; i--) {
+		int maxIndex = 0; // 첫번째 원소
+		for (int j = 1; j <= i; j++) {
+			if (L[maxIndex] < L[j]) {
+				maxIndex = j; // maxIndex값 가장 큰 값이 있는 곳의 index로 바꿔주기
+			}
+			int temp = L[maxIndex]; // 자리 바꿈
+			L[maxIndex] = L[i];
+			L[i] = temp;
+		}
+	}
 }
 
 void main()
